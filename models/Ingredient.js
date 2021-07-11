558bc2209f27c0b078f5cce62552b058a95f5171
Chaz Kondo <chuckeekondo@gmail.com>
2021-07-10 15:22:34 -1000

    edit name

---
+++ models/Ingredient.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+const IngredientSchema = new mongoose.Schema({
+	name: {
+		type: String,
+		required: [true, 'Please set the name']
+	},
+    type: {
+        type: String,
+		required: [true, 'Please set the type']
+    },
+	date: {
+        type: Date,
+        required: [true]
+    },
+	signature: {
+		type: String,
+		required: [true, 'Please provide a signature']
+	},
+})
+
+module.exports = mongoose.models.Ingredient || mongoose.model('Ingredient', IngredientSchema);
