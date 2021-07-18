99829ec67fe84d6cd6aa03b036ba4df27a1937fd
Chaz Kondo <chuckeekondo@gmail.com>
2021-07-18 13:41:49 -1000

    add basic line break

--- pages/recipes.js
+++ pages/recipes.js
@@ -18,6 +18,7 @@ export default function Ingredients() {
   return (
     <div onClick={()=>doACall()} >
         Add Ingredient
+        <br />
         <input type="text" placeholder={'Enter Ingredient Name'} onChange={(e) => setIngredientValue(e.target.value)} value={name}  />
     </div>
   );
