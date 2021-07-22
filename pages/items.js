d8780db4a76c24bb07fe0db8c2d4bf345024f2f8
Chaz Kondo <chuckeekondo@gmail.com>
2021-07-22 09:56:00 -1000

    next implementation

--- pages/items.js
+++ pages/items.js
@@ -123,7 +123,7 @@ export default function Items() {
         <br />
         <input type="text" placeholder={'Enter Recipe Name'} onChange={(e) => setRecipeName(e.target.value)} value={recipeName}  />
         <br />
-        <label htmlFor="items">Choose a type:</label>
+        <label htmlFor="items">Choose a recipe type:</label>
         <select id="items" name="items" onChange={(e) => setDropdownValue(e)} value={type}>
           {ingredientTypes.map((item, i) => <option value={i} key={i}>{item}</option>)}
         </select>
