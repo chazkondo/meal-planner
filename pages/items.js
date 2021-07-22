4191c2d6d98e934b0aba279b332a6c052850958a
Chaz Kondo <chuckeekondo@gmail.com>
2021-07-22 10:03:40 -1000

    fix

--- pages/items.js
+++ pages/items.js
@@ -25,7 +25,7 @@ export default function Items() {
   const [allIngredients, setAllIngredients] = useState([])
   const [ingredientIndex, setIngredientIndex] = useState(0)
   const [allRecipes, setAllRecipes] = useState([])
-  const [recipeDropdown, setRecipeDropdown] = useState(recipeTypes[0])
+  const [recipeDropdown, setRecipeDropdown] = useState(0)
 
   const [currentAmountDiv, setCurrentAmountDiv] = useState(null)
   const [currentAmount, setCurrentAmount] = useState('')
