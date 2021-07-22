e89fe5df61d465f679569d3ee00e589e34318d0f
Chaz Kondo <chuckeekondo@gmail.com>
2021-07-22 09:40:41 -1000

    clean

--- pages/items.js
+++ pages/items.js
@@ -110,7 +110,7 @@ export default function Items() {
         <br />
         <label htmlFor="items">Choose a type:</label>
         <select id="items" name="items" onChange={(e) => setDropdownValue(e)} value={type}>
-          {types.map((item, i) => <option value={i} key={i}>{item}</option>)}
+          {ingredientTypes.map((item, i) => <option value={i} key={i}>{item}</option>)}
         </select>
         <button onClick={()=>postItem()}>Submit</button>
     </div>
