b0c57b2ede783e94fa8a540b458f3ca7a01e333b
Chaz Kondo <chuckeekondo@gmail.com>
2021-07-22 10:07:11 -1000

    next implementation

--- pages/items.js
+++ pages/items.js
@@ -130,7 +130,7 @@ export default function Items() {
           {recipeTypes.map((item, i) => <option value={i} key={i}>{item}</option>)}
         </select>
         <label htmlFor="servings">(Optional) Servings:</label>
-        <input type="text" placeholder={'Enter Recipe Name'} onChange={(e) => setRecipeName(e.target.value)} value={recipeName}  />
+        <input type="number" placeholder={'Enter Amount of Servings'} onChange={(e) => setRecipeName(e.target.value)} value={recipeName}  />
         <br />
         {currentRecipeIngredients.map((recipeItem, mappedIndex) => 
           <ul>
