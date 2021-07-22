78ad25c6b6a76caf72aef4265a0bd2b1a6f2fc2d
Chaz Kondo <chuckeekondo@gmail.com>
2021-07-22 09:39:35 -1000

    create recipe types

--- pages/items.js
+++ pages/items.js
@@ -14,6 +14,7 @@ export default function Items() {
     'Paper Goods – paper towels, toilet paper, aluminum foil, sandwich bags',
     'Personal Care – shampoo, soap, hand soap, shaving cream',
     'Other' ]
+  const recipeTypes = []
   const condensedTypes = ['Beverages', 'Bread/Bakery', 'Canned/Jarred Goods', 'Dairy', 'Dry/Baking Goods', 'Frozen Foods', 'Meat', 'Produce', 'Cleaners', 'Paper Goods', 'Personal Care', 'Other']
   const [name, setName] = useState('')
   const [recipeName, setRecipeName] = useState('')
