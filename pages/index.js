63de85ecc026e0137d38514d06faec2f85d3db79
Chaz Kondo <chuckeekondo@gmail.com>
2021-07-23 12:28:32 -1000

    next implementation

--- pages/index.js
+++ pages/index.js
@@ -23,7 +23,13 @@ const Recipe = memo(({ event }) => {
   });
 
   useEffect(()=>{
-    
+    axios
+    .get('/api/recipes')
+    .then(recipes => {
+      console.log(recipes.data.recipes, 'here')
+      setAllRecipes(recipes.data.recipes)
+    })
+    .catch(err => console.log(err))
   })
 
   return (
