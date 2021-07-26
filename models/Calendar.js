81d7750ee9a57fb4198b2a47d241a15251f821e5
Chaz Kondo <chuckeekondo@gmail.com>
2021-07-25 21:52:21 -1000

    update model structure to more traditional setting

--- models/Calendar.js
+++ models/Calendar.js
@@ -1,13 +1,25 @@
 import mongoose from "mongoose";
 
 const CalendarSchema = new mongoose.Schema({
-	entry : {
-        id: String,
-        color: String,
-        name: String,
-        title: String,
-        _date: Date,
-        _instance: String
+	id: {
+        type: String,
+        required: [true, 'Please provide the ID']
+    },
+    color: {
+        type: String,
+        required: [true, 'Please provide the color hex']
+    },
+    title: {
+        type: String,
+        required: [true, 'Please provide the name/title']
+    },
+    _date: {
+        type: Date,
+        required: [true, 'Please provide the date']
+    },
+    _instance: {
+        type: String,
+        required: [true, 'Please provide the instance']
     },
 	signature: {
 		type: String,
