7906da2adc8a1ec6bfa93c33219aade9de080da4
Chaz Kondo <chuckeekondo@gmail.com>
2021-07-25 21:13:04 -1000

    create a pathway for PUT request

--- pages/api/calendar/index.js
+++ pages/api/calendar/index.js
@@ -55,6 +55,45 @@ export default async function calendarSwitch(req, res){
                 res.status(400).json({success: false})
             }
             break;
+        case 'PUT':
+            try {
+                // const forwarded = req.headers['x-forwarded-for'];
+                // const ip = forwarded ? forwarded.split(/, /)[0] : req.connection.remoteAddress
+                // const existingRecord = await Connections.find({connection: ip})
+                // if (existingRecord.length) {
+                //     if (existingRecord[0].attempt >= 4) {
+                //         return res.status(200).json({success: false, blacklist: true})
+                //     }
+                //     if (req.body.password === process.env.PASSWORD || req.body.password === process.env.PASSWORD1 || req.body.password === process.env.PASSWORD2) {
+                //         let signature;
+                //         if (req.body.password === process.env.PASSWORD) {
+                //             signature = 'Mom'
+                //         } else if (req.body.password === process.env.PASSWORD1) {
+                //             signature = 'Dad'
+                //         } else if (req.body.password === process.env.PASSWORD2) {
+                //             signature = 'Grandma'
+                //         }
+                // if (isConfirmed) {
+
+                console.log(req.body, ' something was sent to the bag end')
+                    // const amount = req.body.ingredients.map(ingredient => ingredient.amount || '')
+                    // const data = {...req.body, password: null, date: Date.now(), signature: 'Chaz', amount}
+                    // const recipes = await Recipe.create(data)
+    
+                    // res.status(201).json({success: true, recipes})
+                // } 
+                // else {
+                //     res.status(400).json({success: false, message: 'Invalid'})
+                // }
+                //     } else {
+                //         res.status(400).json({success: false, message: 'Invalid password'})
+                //     }
+                // }
+            } catch (error) {
+                console.log(error)
+                res.status(400).json({success: false})
+            }
+            break;
         default:
             res.status(400).json({success: false, default: true})
             break;
