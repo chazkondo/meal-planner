bda21fcfd9e7beca17ed2b30e42008eeae15c9e8
Chaz Kondo <chuckeekondo@gmail.com>
2021-07-25 22:21:19 -1000

    test implementation

--- pages/index.js
+++ pages/index.js
@@ -73,7 +73,10 @@ export default function App() {
         setFlag(1)
       }
       for (let i=0; i<calendarEntries.data.calendarEntries.length; i++) {
-        calendarEntries.data.calendarEntries[i]._date = new Date(calendarEntries.data.calendarEntries[i]._date)
+        calendarEntries.data.calendarEntries[i]._date = new Date(calendarEntries.data.calendarEntries[i]._date);
+        calendarEntries.data.calendarEntries[i].start = new Date(calendarEntries.data.calendarEntries[i]._date);
+        calendarEntries.data.calendarEntries[i].end = new Date(calendarEntries.data.calendarEntries[i]._date);
+        calendarEntries.data.calendarEntries[i].name = calendarEntries.data.calendarEntries[i].title
       }
       updateCalendar(calendarEntries.data.calendarEntries)
     })
