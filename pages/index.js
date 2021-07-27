9a94157169c85e6be148bfeb497889218aa81bb5
Chaz Kondo <chuckeekondo@gmail.com>
2021-07-27 12:22:57 -1000

    clean

--- pages/index.js
+++ pages/index.js
@@ -114,7 +114,6 @@ export default function App() {
       console.log(event, ' this should be the correct event now ?')
       console.log(e, ' find the new date please ... e.revert() should be good for axios call')
       // console.log(event, 'the event')
-      // updateCalendarDB(event)
       event._date = e.event.start
       console.log(event, 'did a change occur?')
 
