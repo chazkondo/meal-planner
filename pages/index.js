17b400a10c3432df2545c6368d24eb1b767ba812
Chaz Kondo <chuckeekondo@gmail.com>
2021-07-27 12:36:20 -1000

    debug issue

--- pages/index.js
+++ pages/index.js
@@ -113,9 +113,6 @@ export default function App() {
       let event
       const event = calendar.find(item => item._id === id)
 
-      if (!event) {
-        event = calendar.find(item => item.id === id)
-      }
 
       event._date = e.event.start
       console.log(event, 'test implementation')
