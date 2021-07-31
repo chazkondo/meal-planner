3ee0ab14fb72dd0c383dab2a530ba80efbf1e087
Chaz Kondo <chuckeekondo@gmail.com>
2021-07-31 10:02:22 -1000

    implement price on ingredients and items

--- models/Ingredient.js
+++ models/Ingredient.js
@@ -9,6 +9,10 @@ const IngredientSchema = new mongoose.Schema({
         type: String,
 		required: [true, 'Please set the type']
     },
+	price: {
+		type: Number,
+		required: [false]
+	},
 	date: {
         type: Date,
         required: [true]
