be7a8ee3f74416cf227fb26aad6a1607b20ebd08
Chaz Kondo <chuckeekondo@gmail.com>
2021-07-31 08:26:39 -1000

    fix

---
+++ pages/api/groceryitems/index.js
@@ -0,0 +1,62 @@
+import dbConnect from '../../../utils/dbConnect';
+import Item from '../../../models/Item';
+import Ingredient from '../../../models/Ingredient';
+// import Connections from '../../../models/Connections';
+
+dbConnect();
+
+export default async function groceryItemSwitch(req, res){
+    const {method} = req;
+    // const {isConfirmed} = req.body.result
+
+    switch(method) {
+        case 'GET':
+            try {
+                const items = await Item.find({});
+                res.status(200).json({success: true, items})
+            } catch (error) {
+                res.status(400).json({success: false})
+            }    
+            break;
+        case 'POST':
+            try {
+                // const forwarded = req.headers['x-forwarded-for'];
+                // const ip = forwarded ? forwarded.split(/, /)[0] : req.connection.remoteAddress
+                // const existingRecord = await Connections.find({connection: ip})
+                // if (existingRecord.length) {
+                //     if (existingRecord[0].attempt >= 4) {
+                //         return res.status(200).json({success: false, blacklist: true})
+                //     }
+                //     if (req.body.password === process.env.PASSWORD || req.body.password === process.env.PASSWORD1 || req.body.password === process.env.PASSWORD2) {
+                //         let signature;
+                //         if (req.body.password === process.env.PASSWORD) {
+                //             signature = 'Mom'
+                //         } else if (req.body.password === process.env.PASSWORD1) {
+                //             signature = 'Dad'
+                //         } else if (req.body.password === process.env.PASSWORD2) {
+                //             signature = 'Grandma'
+                //         }
+                // if (isConfirmed) {
+                    const data = {...req.body, password: null, date: Date.now(), signature: 'Chaz'}
+                    console.log(data, 'LETS SEE DATA ')
+                    const items = await Item.create(data);
+    
+                    res.status(201).json({success: true, items})
+                // } 
+                // else {
+                //     res.status(400).json({success: false, message: 'Invalid'})
+                // }
+                //     } else {
+                //         res.status(400).json({success: false, message: 'Invalid password'})
+                //     }
+                // }
+            } catch (error) {
+                console.log(error)
+                res.status(400).json({success: false})
+            }
+            break;
+        default:
+            res.status(400).json({success: false, default: true})
+            break;
+    }
+}
