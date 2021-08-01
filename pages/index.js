d6c55d8322b9614a14bcb5b085ee6abcc2ef31ca
Chaz Kondo <chuckeekondo@gmail.com>
2021-08-01 13:40:00 -1000

    more explicit

--- pages/index.js
+++ pages/index.js
@@ -314,7 +314,7 @@ export default function App() {
             add: {
               text: '+',
               click: function() {
-                add()
+                routeToItemOrRecipePages()
               }
             }
           }}
