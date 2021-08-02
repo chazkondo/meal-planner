425d81de846d0a4bf1aca47c2d1fc208d24d024d
Chaz Kondo <chuckeekondo@gmail.com>
2021-08-02 07:03:07 -1000

    implement alert in call back

--- pages/items.js
+++ pages/items.js
@@ -2,6 +2,7 @@ import axios from "axios";
 import React, { useEffect, useState } from "react";
 import styles from '../styles/item.module.css'
 import { useRouter } from 'next/router'
+import Alert from "sweetalert2";
 
 // Add modals for each function
 
@@ -44,15 +45,45 @@ export default function Items() {
 
 
   function postItem() {
-    alert('sup')
-
     if (type > 7) {
       axios
         .post('/api/items/', {
           name,
           type: condensedTypes[type]
         })
-        .then(res=>console.log(res, 'wat?'))
+        .then(res=>{
+          Alert.fire({
+            title: eventClick.event._def.title + '<div style="font-size: 20">' + eventClick.event.start.toString().slice(0, 15) + '</div>',
+            html: isRecipe ? 
+            `<div>` +
+              getIngredients(event.recipe_id) +
+            '</div>' : null,
+      
+            showCancelButton: true,
+            confirmButtonColor: "#d33",
+            cancelButtonColor: "#3085d6",
+            confirmButtonText: "Remove",
+            cancelButtonText: "Close",
+          }).then((result) => {
+            if (result.value) {
+              const newArr = actualCalendar.filter(events => events._id !== event._id)
+              updateActualCalendar(newArr)
+              // IF event was just added on front end UI
+              if (eventClick.event._def.extendedProps.uuid) {
+                // utilize remove() function
+                eventClick.event.remove(); // It will remove event from the calendar
+                // if uuid exists, then this item has not been added to calendar arr, but is in actualCalendar arr
+                // delete from db first
+                deleteFromCalendarDB(event, eventClick, deleteCallback)
+      
+              } else {
+                const calendarArr = calendar.filter(events => events._id !== event._id)
+                updateCalendar(calendarArr)
+                deleteFromCalendarDB(event, null, deleteCallback)
+              }
+            }
+          });
+        })
         .catch(err=>console.log(err, 'wat err?'))
     } else {
       axios
@@ -60,9 +91,42 @@ export default function Items() {
           name,
           type: condensedTypes[type]
         })
-        .then(res=>console.log(res, 'wat?'))
+        .then(res=>{
+          Alert.fire({
+            title: eventClick.event._def.title + '<div style="font-size: 20">' + eventClick.event.start.toString().slice(0, 15) + '</div>',
+            html: isRecipe ? 
+            `<div>` +
+              getIngredients(event.recipe_id) +
+            '</div>' : null,
+      
+            showCancelButton: true,
+            confirmButtonColor: "#d33",
+            cancelButtonColor: "#3085d6",
+            confirmButtonText: "Remove",
+            cancelButtonText: "Close",
+          }).then((result) => {
+            if (result.value) {
+              const newArr = actualCalendar.filter(events => events._id !== event._id)
+              updateActualCalendar(newArr)
+              // IF event was just added on front end UI
+              if (eventClick.event._def.extendedProps.uuid) {
+                // utilize remove() function
+                eventClick.event.remove(); // It will remove event from the calendar
+                // if uuid exists, then this item has not been added to calendar arr, but is in actualCalendar arr
+                // delete from db first
+                deleteFromCalendarDB(event, eventClick, deleteCallback)
+      
+              } else {
+                const calendarArr = calendar.filter(events => events._id !== event._id)
+                updateCalendar(calendarArr)
+                deleteFromCalendarDB(event, null, deleteCallback)
+              }
+            }
+          });
+        })
         .catch(err=>console.log(err, 'wat err?'))
     }
+    
   }
 
   function setDropdownValue(e) {
