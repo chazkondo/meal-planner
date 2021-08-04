df41a21e6089c05a0138ce402fda497e9d965272
Chaz Kondo <chuckeekondo@gmail.com>
2021-08-04 07:29:25 -1000

    next implementation

--- pages/items.js
+++ pages/items.js
@@ -98,38 +98,7 @@ export default function Items() {
           type: condensedTypes[type]
         })
         .then(()=>{
-          setNavigationBlocker(false)
-          Alert.fire({
-            title: 'sanity',
-            html: `<div>` +
-              'hi' +
-            '</div>',
-      
-            showCancelButton: true,
-            confirmButtonColor: "#d33",
-            cancelButtonColor: "#3085d6",
-            confirmButtonText: "Remove",
-            cancelButtonText: "Close",
-          }).then((result) => {
-            if (result.value) {
-              const newArr = actualCalendar.filter(events => events._id !== event._id)
-              updateActualCalendar(newArr)
-              // IF event was just added on front end UI
-              if (eventClick.event._def.extendedProps.uuid) {
-                // utilize remove() function
-                eventClick.event.remove(); // It will remove event from the calendar
-                // if uuid exists, then this item has not been added to calendar arr, but is in actualCalendar arr
-                // delete from db first
-                deleteFromCalendarDB(event, eventClick, deleteCallback)
-      
-              } else {
-                const calendarArr = calendar.filter(events => events._id !== event._id)
-                updateCalendar(calendarArr)
-                deleteFromCalendarDB(event, null, deleteCallback)
-              }
-              router.push('/index') 
-            }
-          });
+          toggleAddMoreAlert()
         })
         .catch(err=>{
           setNavigationBlocker(false)
