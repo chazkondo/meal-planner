94eff304457cf33b0755da2e50cd315a1a8c7b1d
Chaz Kondo <chuckeekondo@gmail.com>
2021-08-07 09:53:47 -1000

    clean

--- pages/api/groceryitems/index.js
+++ pages/api/groceryitems/index.js
@@ -35,35 +35,3 @@ export default async function groceryItemSwitch(req, res){
     }
 }
 
-export const deleteItem = async (req, res) => {
-    console.log(req.query, 'um what is happening?')
-    await dbConnect();
-  
-    const mongooseSession = await mongoose.startSession();
-        
-    try {
-      mongooseSession.startTransaction();
-
-      const {_id, signature} = req.query
-  
-      await Calendar.findOneAndDelete({ _id });
-  
-      await mongooseSession.commitTransaction();
-      mongooseSession.endSession();
-  
-      res.status(200).json({
-        success: true,
-        message: "Successful.",
-      });
-    } catch (err) {
-      console.log("ERROR?", err.message);
-  
-      await mongooseSession.abortTransaction();
-      mongooseSession.endSession();
-  
-      res.status(400).json({
-        success: false,
-        message: "Failed to delete item.",
-      });
-    }
-  };
